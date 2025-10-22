fddfad3de38775d49c8b8f16074bfd6e8d3179e7
Akher <akakher100@gmail.com>
2025-10-22 19:12:36 +0600

    Write a program that takes two integers as input and prints their sum.
    Implement a program to sum two integers

---
+++ 2.c
@@ -0,0 +1,12 @@
+//Write a program that takes two integers as input and prints their sum. 
+#include <stdio.h>
+int main()
+{
+      int a, b, c;
+      printf("Enter two number: ");
+      scanf("%d%d",&a,&b);
+      c = a + b;
+      printf("Sum = %d", c);
+      return 0;
+}
+
