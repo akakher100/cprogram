3ea6a797649f813afeac6c3bfda91a8f9902e5d2
Akher <akakher100@gmail.com>
2025-10-22 20:57:48 +0600

    Write a program to print your name, department, and university.

--- 1.c
+++ 1.c
@@ -1,8 +1,8 @@
 //Write a program to print your name, department, and university. 
 
 #include <stdio.h>
-int main()
-{
+int main(){
+      
       printf("Name: Md. Ikramul Islam Akher\n");
       printf("Department: CSE\n");
       printf("University: Green University of Bangladesh\n");
